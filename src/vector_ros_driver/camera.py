49e4ed65438bc2186b26c3c2af3400be4989a6e3
Ryo Sakagami <sakagamiry@gmail.com>
2020-04-26 18:39:08 +0200

    feat: added camera calibration data

--- src/vector_ros_driver/camera.py
+++ src/vector_ros_driver/camera.py
@@ -1,28 +1,61 @@
 #!/usr/bin/env python3.6
 
 import rospy
+import rospkg
 import anki_vector
 import cv_bridge
 import numpy
+import yaml
 
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 
 class Camera(object):
     def __init__(self, async_robot, publish_rate=10, image_frame_id='camera_link'):
         self.async_robot = async_robot
         self.rate = rospy.Rate(publish_rate)
         self.image_frame_id = image_frame_id
-        self.image_publisher = rospy.Publisher("~camera", Image, queue_size=1)
+
+        rospack = rospkg.RosPack()
+        pkg_path = rospack.get_path('vector_ros_driver')
+
+        # Camera calibration file
+        camera_calibration_path = rospy.get_param(
+            '~camera_calibration_path', pkg_path + "/configs/camera_calibration.yaml")
+        with open(camera_calibration_path, "r") as file_handle:
+            calib_data = yaml.load(file_handle)
+
+        # CV bridge
+        self.bridge = cv_bridge.CvBridge()
+
+        # Publishers
+        self.image_raw_publisher = rospy.Publisher("~camera/image_raw", Image, queue_size=1)
+        self.camera_info_publisher = rospy.Publisher("~camera/camera_info", CameraInfo, queue_size=1)
+
+        # Camera information
+        self.camera_info = CameraInfo()
+        self.camera_info.header.frame_id = self.image_frame_id
+        self.camera_info.height = calib_data["image_height"]
+        self.camera_info.width = calib_data["image_width"]
+        self.camera_info.K = calib_data["camera_matrix"]["data"]
+        self.camera_info.D = calib_data["distortion_coefficients"]["data"]
+        self.camera_info.R = calib_data["rectification_matrix"]["data"]
+        self.camera_info.P = calib_data["projection_matrix"]["data"]
+        self.camera_info.distortion_model = calib_data["camera_model"]
+
+
+        # Start publishing camera topics
         self.publish_camera_feed()
 
     def publish_camera_feed(self):
-        bridge = cv_bridge.CvBridge()
+
 
         while not rospy.is_shutdown():
-            image = bridge.cv2_to_imgmsg(numpy.asarray(self.async_robot.camera.latest_image), encoding="rgb8") # convert PIL.Image to ROS Image
+            image = self.bridge.cv2_to_imgmsg(numpy.asarray(self.async_robot.camera.latest_image), encoding="rgb8") # convert PIL.Image to ROS Image
+            self.camera_info.header.stamp = rospy.Time.now()
             image.header.stamp = rospy.Time.now()
             image.header.frame_id = self.image_frame_id
-            self.image_publisher.publish(image)
+            self.image_raw_publisher.publish(image)
+            self.camera_info_publisher.publish(self.camera_info)
 
             # make sure to publish at required rate
             self.rate.sleep()
