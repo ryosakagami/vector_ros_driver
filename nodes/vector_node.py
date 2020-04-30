20ba18beacc45d32f65ed99be2299110c6a2b5fd
Ryo Sakagami <sakagamiry@gmail.com>
2020-04-30 02:06:04 +0200

    Moved thread starting lines into main func

--- nodes/vector_node.py
+++ nodes/vector_node.py
@@ -14,21 +14,8 @@ from vector_ros_driver.behavior import Behavior
 from vector_ros_driver.tf import JointStatesPublisher
 from vector_ros_driver.nav_map import NavMap
 
-if __name__=="__main__":
-    rospy.init_node("vector")
 
-    # use mock robot object if required
-    if rospy.get_param("~use_mock", False):
-        rospy.loginfo("using mock!")
-        sys.path.append(os.path.join(os.path.dirname(__file__), "..", "test"))
-        import mock_robot
-        async_robot = mock_robot.MockRobot()
-
-    else:
-        with anki_vector.AsyncRobot() as async_robot:
-            async_robot.camera.init_camera_feed()
-            async_robot.nav_map.init_nav_map_feed()
-
+def main(async_robot):
     # connect to Vector
     async_robot.connect()
 
@@ -51,3 +38,21 @@ if __name__=="__main__":
     nav_map_thread.start()
 
     rospy.spin()
+
+
+if __name__=="__main__":
+    rospy.init_node("vector")
+
+    # use mock robot object if required
+    if rospy.get_param("~use_mock", False):
+        rospy.loginfo("using mock!")
+        sys.path.append(os.path.join(os.path.dirname(__file__), "..", "test"))
+        import mock_robot
+        async_robot = mock_robot.MockRobot()
+        main(async_robot)
+
+    else:
+        with anki_vector.AsyncRobot() as async_robot:
+            async_robot.camera.init_camera_feed()
+            async_robot.nav_map.init_nav_map_feed()
+            main(async_robot)
